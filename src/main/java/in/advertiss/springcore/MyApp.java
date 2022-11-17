0040b0c3fa2f37fdfa8c97154ca2e38becf0def9
badrufun <38443285+badrufun@users.noreply.github.com>
2022-11-18 01:42:14 +0400

    configuring spring context and getting bean from container using xml

--- src/main/java/in/advertiss/springcore/MyApp.java
+++ src/main/java/in/advertiss/springcore/MyApp.java
@@ -1,10 +1,16 @@
 package in.advertiss.springcore;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MyApp {
     public static void main(String[] args){
         //Coach theCoach = new BaseBallCoach();
         Coach theCoach = new TrackCoach(); //Using any of coach object
         //But object is not configurable
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        //based on configuration file the coach object will be created
+        theCoach = context.getBean("theCoach", Coach.class);
         System.out.println(theCoach.getDailyWorkout());
     }
 }
