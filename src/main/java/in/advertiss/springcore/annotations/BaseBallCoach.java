d0725d8b6f456bb0f11b75cdfbba26e354860762
badrufun <38443285+badrufun@users.noreply.github.com>
2022-11-20 11:47:31 +0400

    constructor, feild and setter injection in same bean

--- src/main/java/in/advertiss/springcore/annotations/BaseBallCoach.java
+++ src/main/java/in/advertiss/springcore/annotations/BaseBallCoach.java
@@ -8,16 +8,17 @@ import org.springframework.stereotype.Component;
 public class BaseBallCoach implements Coach {
 
     //Feild injection --> internally done using java reflection
+    //excutes after constructor injection then setter injection
     @Autowired
     @Qualifier("goodFortuneService")
     private FortuneService fortuneService;
 
     //Auto-injection using constructor happening
-   /* @Autowired
-    public BaseBallCoach(FortuneService fortuneService) {
+    @Autowired
+    public BaseBallCoach(@Qualifier("happyFortuneService") FortuneService fortuneService) {
         this.fortuneService = fortuneService;
         System.out.println("Constructor is called for fortune service injection!!");
-    }*/
+    }
 
     public String getDailyWorkout(){
         return "Practice 30 mins daily!";
@@ -31,9 +32,11 @@ public class BaseBallCoach implements Coach {
     //Auto-injection happening using setter
     //if Auto constructor is also there, then first it will be injected by constructor then by setter
     //Setter injection will override the constructor injection
-   /* @Autowired
+    //Also feild inject
+    @Autowired
+    @Qualifier("happyFortuneService")
     public void setFortuneService(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
         System.out.println("Setter is called for fortune service injection!!");
-    }*/
+    }
 }
